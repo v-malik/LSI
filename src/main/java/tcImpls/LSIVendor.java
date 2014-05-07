0ab087dd4bee761e56ba3456500a912f9da01359
vmalik5005 <vmalik5005@gmail.com>
2014-05-06 22:39:30 -0400

    Changes include the UseCase 3 to include any arbitrary values range. Though this case has no bearing on the unit test, the tests were run to make sure it doesn't break any existing functionality.

--- src/main/java/tcImpls/LSIVendor.java
+++ src/main/java/tcImpls/LSIVendor.java
@@ -2,22 +2,29 @@ package tcImpls;
 
 public class LSIVendor implements VendorFee {
 
+	// to include any arbitrary value. The values could also be separated in a properties
+	// file or a resource bundle
+	public static final double PREFER = 500000;
+	public static final double GENERAL = 100000;
+
 	double fee = 0.00;
 	double cummulativeFee = 0.00;
 
-	/* Calculates vendor fees depending upon the project amount
-	 * introduces varied fee percentage based on the amount range of the vendor project
+	/*
+	 * Calculates vendor fees depending upon the project amount introduces
+	 * varied fee percentage based on the amount range of the vendor project
 	 */
 	public double calcFee(double amt) {
 
-		if (amt <= 100000) {
+		if (amt <= GENERAL) {
 			fee = 0.2 * amt;
 			cummulativeFee = fee;
-		} else if (amt > 100000 && amt < 500000) {
-			fee = (100000 * 0.2) + (amt - 100000) * 0.1;
+		} else if (amt > GENERAL && amt < PREFER) {
+			fee = (GENERAL * 0.2) + (amt - GENERAL) * 0.1;
 			cummulativeFee = fee;
-		} else if (amt > 500000) {
-			fee = (100000 * 0.2) + (500000 - 100000) * 0.1 + (amt - 500000) * 0.05;
+		} else if (amt > PREFER) {
+			fee = (GENERAL * 0.2) + (PREFER - GENERAL) * 0.1 + (amt - PREFER)
+					* 0.05;
 			cummulativeFee = fee;
 		}
 		return cummulativeFee;
