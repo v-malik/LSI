78f4ec0df337b77cff07bc6b78b4a402b41497c9
vmalik5005 <vmalik5005@gmail.com>
2014-05-07 10:32:13 -0400

    Chnages made as per UseCase 3 (fresh inputs from Mr. Chirita).

--- src/main/java/tcImpls/LSIVendor.java
+++ src/main/java/tcImpls/LSIVendor.java
@@ -2,18 +2,13 @@ package tcImpls;
 
 public class LSIVendor implements VendorFee {
 
-	// to include any arbitrary value. The values could also be separated in a properties
-	// file or a resource bundle
-	public static final double PREFER = 500000;
-	public static final double GENERAL = 100000;
-
-	double fee, cummulativeFee, interimCalc, currentFee, totalTargetAmt, freshFee = 0.00;
-
 	/*
-	 * to calculate the fee for a new invoice assuming an existing cost basis, where total invoice
-	 * amount already paid to date is called cost basis.
-	*/
+	 * to calculate the fee for a new invoice assuming an existing cost basis,
+	 * where total invoice amount already paid to date is called cost basis.
+	 */
 	public double calcFeeOnToDateBasis(double toDateAmt, double currentAmt) {
+
+		double interimCalc, currentFee, totalTargetAmt, freshFee = 0.00;
 
 		totalTargetAmt = toDateAmt + currentAmt;
 
@@ -31,16 +26,25 @@ public class LSIVendor implements VendorFee {
 	 */
 	public double calcFee(double amt) {
 
-		if (amt <= GENERAL) {
-			fee = 0.2 * amt;
-			cummulativeFee = fee;
-		} else if (amt > GENERAL && amt < PREFER) {
-			fee = (GENERAL * 0.2) + (amt - GENERAL) * 0.1;
-			cummulativeFee = fee;
-		} else if (amt > PREFER) {
-			fee = (GENERAL * 0.2) + (PREFER - GENERAL) * 0.1 + (amt - PREFER)
-					* 0.05;
-			cummulativeFee = fee;
+		double cummulativeFee = 0.00;
+
+		// to include any arbitrary value ranges
+		double infinity = Double.POSITIVE_INFINITY;
+		double[] range = { 0, 100000, 500000, 800000, 1000000, 2500000,
+				infinity };
+		double[] percentRate = { 0.20, 0.10, 0.05, 0.025, 0.010 };
+
+		int x, i;
+
+		for (i = 0; i <= 5; i++) {
+			if (amt > range[i] && amt <= range[i + 1]) {
+				for (x = 0; x < i; ++x) {
+					cummulativeFee = cummulativeFee + (range[x + 1] - range[x])
+							* percentRate[x];
+				}
+				cummulativeFee = cummulativeFee + (amt - range[i])
+						* percentRate[i];
+			}
 		}
 		return cummulativeFee;
 	}
